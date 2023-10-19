968bd7b69b38f9d4b44ce3ca15d6d29d61087589
Gislaine de Aguiar Guimaraes <gis_guimaraes@hotmail.com>
2023-10-19 12:43:46 +0200

    chamgement logo

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,12 +1,10 @@
 import styles from './Footer.module.css'
+import Logo from '../../images/logo-ecole.png'
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <img
-        src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png"
-        alt="Logo Wild Code School"
-      />
+      <img src={Logo} alt="Logo Wild Code School" />
       <div>
         <p>
           <span>Réalisé par Gislaine de Aguiar Guimarães </span>
